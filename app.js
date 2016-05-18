26af9bc3e51ede53fe847b9a3f0b09fee5e92c11
alu0100693737 <alu0100693737@ull.edu.es>
2016-05-18 20:11:50 +0200

    Creando elementos con npm

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.render('index', { title: 'Proyecto' });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Node app is running at localhost: ${app.get('port')}` );
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
