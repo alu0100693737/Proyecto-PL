c68859e62ec7d3f1431e716dcaa006fc9d341567
alu0100693737 <alu0100693737@ull.edu.es>
2016-05-20 01:38:14 +0200

    Gulp y karma funcionando correctamente

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+gutil = require('gulp-util'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat');
+var del = require('del');
+var karma = require('gulp-karma');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('minify', function () {
+  gulp.src('./public/js/csv.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./public/css/*.css')
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(gulp.dest('./minified/'))
+  });
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./minified/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
