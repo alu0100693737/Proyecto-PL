197443c73db667c33721e21fa80e55e97ca12842
alu0100693737 <alu0100693737@ull.edu.es>
2016-06-01 20:10:59 +0200

    Cambiando navegadores

--- karma.conf.js
+++ karma.conf.js
@@ -64,8 +64,17 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['Firefox', 'ChromeCanary', 'PhantomJS', 'Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
+    if(process.env.TRAVIS){
+     browsers = ['Chrome_travis_ci'];
+  }
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
