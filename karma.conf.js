4a989e0b3592fcb1683a3b88189cd29cf853abb8
alu0100693737 <alu0100693737@ull.edu.es>
2016-06-01 20:30:05 +0200

    Cambiando configuracion karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Wed May 18 2016 20:37:55 GMT+0200 (Hora de verano romance)
+// Generated on Mon Apr 04 2016 18:33:56 GMT+0200 (Hora de verano romance)
 
 module.exports = function(config) {
   config.set({
@@ -8,80 +8,74 @@ module.exports = function(config) {
     basePath: '',
 
 
-    frameworks: ['mocha', 'chai'],
 
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
     client: {
-         mocha: {
-           ui: 'tdd'
-         }
-   },
+      mocha: {
+        ui: 'tdd'
+      }
+    },
 
 
     // list of files / patterns to load in the browser
     files: [
-      'public/test/mocha.js',
-      'public/test/chai.js',
-      'public/test/sinon-1.7.1.js',
       'public/test/pl0tests.js',
-      'public/test/test.js'
-    ],
+      'public/test/mocha.js',
 
+      'public/test/test.js'
+
+    ],
 
     // list of files to exclude
     exclude: [
-
+      'gulpfile.js'
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    },
 
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'ChromeCanary', 'PhantomJS', 'Chrome'],
+    browsers: ['Firefox', 'TravisCIChrome'],
     customLaunchers: {
-      Chrome_travis_ci: {
+      TravisCIChrome: {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
     },
 
-    if(process.env.TRAVIS){
-     browsers = ['Chrome_travis_ci'];
-  }
+   // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+  });
+  if (process.env.TRAVIS) {
+    config.browsers = ['PhantomJS', 'Firefox', 'chromeTravisCI'];
+  }
+};
